646d8a2b9a5b6949cc8dd8cd0d99c7553280419b
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-07-06 17:16:37 +0800

    Image组件

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React from 'C:/Users/xiawa/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Flex from './components/Flex';
+// import Flex from './components/Flex';
 // import Touch from './components/Touch';
-// import ImageBox from './components/ImageBox';
+import ImageBox from './components/ImageBox';
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Flex />
+				<ImageBox />
 			</View>
 		);
 	}
--- components/ImageBox.js
+++ components/ImageBox.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, ImageBackground } from 'react-native';
 
 export default class ImageBox extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
+				{/* <Image
+					source={require('./img.jpg')}
+					style={{
+						width: Dimensions.get('window').width,
+						height: 200,
+						resizeMode: 'center'
+					}}
+				/> */}
 				<Image
-					style={{ width: 60, height: 60 }}
-					source={{ uri: 'http://img5.imgtn.bdimg.com/it/u=415293130,2419074865&fm=27&gp=0.jpg' }}
+					source={{ uri: 'https://img.alicdn.com/tps/TB1OvT9NVXXXXXdaFXXXXXXXXXX-520-280.jpg' }}
+					style={styles.imgStyle}
 				/>
+				<ImageBackground
+					source={{ uri: 'https://img.alicdn.com/tps/TB1OvT9NVXXXXXdaFXXXXXXXXXX-520-280.jpg' }}
+					style={styles.imgStyle}
+				>
+					<Text style={styles.textStyle}>Image组件不能放子组件，需要用ImageBackground</Text>
+				</ImageBackground>
+				{/* <Text>{Dimensions.get('window').width}</Text> */}
 			</View>
 		);
 	}
@@ -20,5 +35,16 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'space-around'
+	},
+	imgStyle: {
+		width: Dimensions.get('window').width,
+		height: 200,
+		justifyContent: 'center'
+	},
+	textStyle: {
+		padding: 10,
+		textAlign: 'center',
+		color: '#fff',
+		backgroundColor: '#0081dc'
 	}
 });
